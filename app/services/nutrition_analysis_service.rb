81064231d0cc3c923683d64877232bc0d895187c
Sebastian Burke <burke.sebastian1@gmail.com>
2024-09-05 14:30:13 +0800

    home page design updates + color scheme udpate

--- app/services/nutrition_analysis_service.rb
+++ app/services/nutrition_analysis_service.rb
@@ -23,7 +23,7 @@ class NutritionAnalysisService
 
   def nutrition_prompt(user_data)
     <<~PROMPT
-      Create a 3-day nutrition plan for a #{user_data.age}-year-old #{user_data.gender} weighing #{user_data.current_weight} pounds who works out #{user_data.activity_level} times per week, primarily doing #{user_data.workout_type}. Goal is to #{user_data.fitness_goal}. Food preferences are #{user_data.food_preferences}. Provide #{user_data.count_meals} meals with #{user_data.snacks ? 'some' : 'no'} snacks. Include daily totals for calories, proteins, carbohydrates, and fats. Exclude any introductory text and special characters.
+      Create a 3-day nutrition plan for a #{user_data.age}-year-old #{user_data.gender} weighing #{user_data.current_weight} pounds who works out #{user_data.activity_level} times per week, primarily doing #{user_data.workout_type}. Goal is to #{user_data.fitness_goal}. Food preferences are #{user_data.food_preferences}. Provide #{user_data.count_meals} meals with #{user_data.snacks ? 'some' : 'no'} snacks. Include daily totals for calories, proteins, carbohydrates, and fats. Daily calories should never be less than 1800. Exclude any introductory text and special characters.
     PROMPT
   end
 end
